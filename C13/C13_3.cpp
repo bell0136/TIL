c0c2ca4d0cf7f5d433e9cf499eae7cbe57d81708
bell0136 <57171242+bell0136@users.noreply.github.com>
2019-11-24 22:00:45 +0900

    2019_11_24

---
+++ C13/C13_3.cpp
@@ -0,0 +1,14 @@
+#include <stdio.h>
+int main(void)
+{
+	int i = 0;
+	int arr[5] = { 1, 2, 3, 4, 5 };
+	int* ptr = &arr[4];
+	int sum = 0;
+	for (i = 0; i < 5; i++)
+	{
+		sum += *(ptr - i);
+	}
+	printf("รัวี %d", sum);
+}
+
