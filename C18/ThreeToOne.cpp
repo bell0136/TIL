5836e4549e5acafcad2a240778fb1b903b0b1271
bell0136 <57171242+bell0136@users.noreply.github.com>
2020-02-07 18:43:20 +0900

    2020_02_07

---
+++ C18/ThreeToOne.cpp
@@ -0,0 +1,69 @@
+#include <stdio.h>
+int *d_change(int (* 3array)[][][],int *order)
+{
+	int high, sero, garo, one = 0;
+	if (3array[0] == 0)
+	{
+		if (3array[1] == 1)
+		{
+			if (3array[2] == 2)
+			{
+				for (high = 0; high < 2; high++)
+				{
+					for()
+				}
+			}
+		}
+		if (3array[1] == 2)
+		{
+			if (3array[2] == 1)
+			{
+				for
+			}
+		}
+
+	}
+	if (3array[0] == 1)
+	{
+		if (3array[1] == 0)
+		{
+			if (3array[2] == 2)
+			{
+			}
+		}
+		if (3array[1] == 2)
+		{
+			if (3array[2] == 0)
+			{
+			}
+		}
+
+	}
+	if (3array[0] == 2)
+	{
+		if (3array[1] == 1)
+		{
+			if (3array[2] == 0)
+			{
+			}
+		}
+		if (3array[1] == 0)
+		{
+			if (3array[2] == 1)
+			{
+			}
+		}
+
+	}
+}
+int main(void)
+{
+	int 3darr[2][3][4] =
+	{
+		{{},{},{}},{{},{},{}}
+	};
+	int arr[3] = { ,, };
+
+	to_3d(3darr, arr);
+
+}
