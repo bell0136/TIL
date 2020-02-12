2455b9317022ccdaca3149c10d55138e674d19fb
bell0136 <57171242+bell0136@users.noreply.github.com>
2020-02-12 09:58:09 +0900

    2020_0212

--- C18/ThreeToOne.cpp
+++ C18/ThreeToOne.cpp
@@ -1,69 +1,46 @@
 #include <stdio.h>
-int *d_change(int (* 3array)[][][],int *order)
+int* ThreeToOne(int* depth,int* 3darr)
 {
-	int high, sero, garo, one = 0;
-	if (3array[0] == 0)
+	int i = 0;
+	int j = 0;
+	int k = 0;
+	int l = 0;
+	int 1darr[12] = { 0 };
+	for (i = 0; i <depth[2];i++)
 	{
-		if (3array[1] == 1)
+		for (j = 0; j<depth[1];j++)
 		{
-			if (3array[2] == 2)
+			for (k=0; k<depth[0]; k++)
 			{
-				for (high = 0; high < 2; high++)
-				{
-					for()
-				}
+				printf("%d ", 3darr[k][j][i]);
 			}
 		}
-		if (3array[1] == 2)
-		{
-			if (3array[2] == 1)
-			{
-				for
-			}
-		}
-
 	}
-	if (3array[0] == 1)
-	{
-		if (3array[1] == 0)
-		{
-			if (3array[2] == 2)
-			{
-			}
-		}
-		if (3array[1] == 2)
-		{
-			if (3array[2] == 0)
-			{
-			}
-		}
 
-	}
-	if (3array[0] == 2)
-	{
-		if (3array[1] == 1)
-		{
-			if (3array[2] == 0)
-			{
-			}
-		}
-		if (3array[1] == 0)
-		{
-			if (3array[2] == 1)
-			{
-			}
-		}
-
-	}
+	return 1darr;
 }
 int main(void)
 {
-	int 3darr[2][3][4] =
+	int arr[3] = { 2,1,0 };
+	int i = 0;
+	int 3darr[2][2][3] = 
 	{
-		{{},{},{}},{{},{},{}}
-	};
-	int arr[3] = { ,, };
+		{
+			{1,2,3},
+			{4,5,6}
+		},
+		{
+			{7,8,9},
+			{10,11,12}
+		}
+	};//높이 = 1,세로 = 2, 가로 = 3
 
-	to_3d(3darr, arr);
+	ThreeToOne(arr, 3darr);
 
+	/*int* ptr = 1darr;
+
+	for (i = 0; i < 12; i++)//배열의 길이
+	{
+		printf("%d ", *(ptr + i));
+	}*/
 }
--- C20/C20_Q6.cpp
+++ C20/C20_Q6.cpp
@@ -5,36 +5,51 @@ int main(void)
 {
 	int i = 0;
 	int j = 0;
+	int k = 0;
 	srand((int)time(NULL));
 	int random[3] = { rand() % 9,rand() % 9,rand() % 9 };
 	int num[3] = { 0 };
 
 	int strike = 0;
 	int ball = 0;
-
+	
 	printf("Start Game\n");
 	for (j = 0; j < 3; j++)
 	{
-		printf("%d", random[i]);
+		printf("%d", random[j]);
 	}
 	printf("\n");
-	for (i = 0; i < 3; i++)
+
+	j = 0;
+
+	while (1)
 	{
-		printf("3개의 숫자 선택 : \n");
-		scanf("%d %d %d", &num[0], &num[1], &num[2]);
-		/*if (random[i] == num[i])
+		strike = 0;
+		ball = 0;
+		printf("\n3개의 숫자선택:");
+		scanf("%d %d %d", &num[0], &num[1], &num[2]);	
+		for (i = 0; i < 3; i++)
 		{
-			strike += 1;
-		}
-		if (random == num);
-		{
-			ball += 1;
-		}
-		printf("%d strike, %d ball\n", strike, ball);
-		*/
-		if (strike == 3)
-		{
-			printf("Game Over!\n");
-		}		
-	}
+			for (j = 0; j < 3; j++)
+			{
+				if ( i == j && num[i] == random[j] )
+				{
+					strike += 1;
+					if (strike == 3)
+					{
+						printf("\n%d번째 도전 결과: 3 strike! \n", k + 1 );
+						printf("Game Over\n");
+						return 0;
+					}
+				}
+				else if  (i != j && num[i] == random[j])
+				{
+						ball += 1;
+				}
+			}
+		}	
+
+		printf("\n%d번째 도전 결과: %d strike, %d ball\n", k+1, strike, ball);
+		k++;
+	}	
 }
