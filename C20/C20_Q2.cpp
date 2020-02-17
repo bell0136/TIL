3336570abae47252877dbcea115c1bac8354d6b3
bell0136 <57171242+bell0136@users.noreply.github.com>
2020-02-17 10:29:09 +0900

    2020_02_17

--- C20/C20_Q2.cpp
+++ C20/C20_Q2.cpp
@@ -1,38 +1,43 @@
 #include<stdio.h>
-void snail(int side_length, int turn)
+void snail(int side_length)
 {
 	int num = 1;
 	int count_garo = 0;
-	int count_sero = 0;	
-
+	int count_sero = 0;
 	static int arr[50][50] = { 0 };
-
 	int k = 0;
 	int idx_sero = 0;
 	int idx_garo = 0;
+	int size = 0;
 
-	while (k < side_length-2)
-	{		
-		for (count_garo = k; count_garo < side_length-k; count_garo++) //가로 -->
-		{			
+	while (1)
+	{
+		if (k == side_length-1) 
+		//원래는 k==side_length-2였다 두 cycle에 한번씩 올라가므로(가로,세로,가로,세로) 근데 side_length -2하면 3,2가 출력이 안돼서 -1로 수정/ 2,3을 제외한 나머지숫자에서는 불리한 코드..  
+		{
+			break;
+		}
+		size = side_length - k;
+		for (count_garo = k; count_garo < size; count_garo++) //가로 --> ,처음에 한변의 길이 만큼
+		{
 			arr[k][count_garo] = num;
 			num++;
-		}		
-		for (count_sero = k+1; count_sero < side_length-k; count_sero++) //세로밑으로
-		{			
-			arr[count_sero][(side_length-k)-1] = num;
+		}
+		for (count_sero = k + 1; count_sero < size; count_sero++) //세로밑으로 count_sero 횟수 한번 줄어듦 (k+1,범위는 다르게)
+		{
+			arr[count_sero][size - 1] = num;
 			num++;
-		}	
-		for (count_garo = (side_length-k)-2; count_garo >= k; count_garo--) //가로 <--
-		{			
-			arr[(side_length-k)-1][count_garo] = num;
+		}
+		for (count_garo = size - 1; count_garo > k; count_garo--) //가로 <-- 횟수 그대로 count_garo줄어듦 인덱스도 한번 줄어드니까 [count_garo-1]하기
+		{
+			arr[size - 1][count_garo - 1] = num;
 			num++;
-		}		
-		for (count_sero = (side_length-k)-2; count_sero > k; count_sero--) //세로위로
-		{			
+		}
+		for (count_sero = size - 2; count_sero > k; count_sero--) //세로위로 count_sero 횟수 한번 줄어듦
+		{
 			arr[count_sero][k] = num;
 			num++;
-		}		
+		}
 		k++;
 	}
 
@@ -52,6 +57,5 @@ int main(void)
 	int side_length = 0; //한변의 길이
 	printf("숫자를 입력하시오:");
 	scanf("%d", &side_length);
-	int turn_result = 2 * side_length - 1; //도는 횟수
-	snail(side_length, turn_result);
+	snail(side_length);
 }
