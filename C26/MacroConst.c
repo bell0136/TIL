b261324f3db80eb826d04d185ceef38b5cd614f7
bell0136 <57171242+bell0136@users.noreply.github.com>
2020-02-24 18:36:43 +0900

    2020_02_24

---
+++ C26/MacroConst.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+#define name	"홍길동"
+#define age	24		
+#define address		puts("부산시 사상구");
+
+int main()
+{
+	printf("이름 %s\n", name);
+	printf("나이 %d\n", age);
+	address;
+	return 0;
+}
