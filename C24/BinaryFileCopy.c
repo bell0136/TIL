d212b68703ac25684e1c55135e5c464c5dde186f
bell0136 <57171242+bell0136@users.noreply.github.com>
2020-03-02 17:54:19 +0900

    2020_03_02

--- C24/BinaryFileCopy.c
+++ C24/BinaryFileCopy.c
@@ -1,11 +1,10 @@
 #include <stdio.h>
 #pragma warning (disable : 4996)
-
 int main()
 {
-	FILE* src = fopen("src.bin", "rt");
-	FILE* des = fopen("des.bin", "wt");
-	char buf[20];
+	FILE* src = fopen("src.bin", "rb");
+	FILE* des = fopen("des.bin", "wb");
+	char buf[5] = {1,2,3,4,5};
 	int read;
 
 	if (src == NULL || des == NULL)
@@ -14,26 +13,26 @@ int main()
 		return -1;
 	}
 
+	
 	while (1)
 	{
 		read = fread((void*)buf, 1, sizeof(buf), src);
-		if(read < sizeof(buf))
+		if(read < sizeof(buf)) 
+		//다 못 읽거나, 파일의 끝에 도달했을 때
 		{		
-			if (feof(src) != 0)
+			if (feof(src) != 0)//파일의 끝에 도달
 			{
 				fwrite((void*)buf, 1, read, des);
+				//읽은만큼 des파일에 쓴다.
 				puts("파일복사성공");
-				break;
-				
+				break;			
 			}
-			else
+			else // 에러 일 경우
 			{
 				puts("파일복사실패");
-
 			}
 		}
-		fwrite((void*)buf, 1, sizeof(buf), des);
-		
+		fwrite((void*)buf, 1, sizeof(buf), des);//데이터가 꽉찰때		
 	}
 	fclose(src);
 	fclose(des);
