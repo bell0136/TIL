5edec481140f3c14fd1f9d658914443f183d8d4e
bell0136 <57171242+bell0136@users.noreply.github.com>
2020-03-28 02:31:54 +0900

    2020_03_28

--- C10/도전 2번.cpp
+++ C10/도전 2번.cpp
@@ -4,29 +4,26 @@ int main(void)
 	int dan1;//피승수 (from)
 	int dan2;//피승수 (to) 
 	int is = 1;//승수 
+	int n = 0;
+	int k = 0;
 	printf("두개의 정수를 입력하시오.\n");
 	scanf("%d %d", &dan1, &dan2);
 	if (dan1 < dan2)
 	{
-		for (; dan1 <= dan2; dan1++)
-		{
-
-			for (is = 1; is < 10; is++)
-			{
-				printf("%dX%d=%d\n", dan1, is, dan1 * is);
-			}
-		}
+		k = dan1;
+		n = dan2; //큰수
 	}
-	else if (dan2 < dan1)
+	if (dan2 < dan1)
 	{
-		for (; dan2 <= dan1; dan2++)
-		{
-
-			for (is = 1; is < 10; is++)
-			{
-				printf("%dX%d=%d\n", dan2, is, dan2 * is);
-			}
-		}
+		k = dan2;
+		n = dan1;
 	}
 
+	for (k; k <= n; k++)
+	{
+		for (is = 1; is < 10; is++)
+		{
+			printf("%d X %d = %d\n", k, is, k * is);
+		}
+	}
 }
