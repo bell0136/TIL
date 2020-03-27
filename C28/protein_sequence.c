27076fcfb882a16bb3652352ba061e7a64ad6d6e
bell0136 <57171242+bell0136@users.noreply.github.com>
2020-03-28 02:00:24 +0900

    2020_03_282020_03_28

--- C28/protein_sequence.c
+++ C28/protein_sequence.c
@@ -4,6 +4,22 @@ void delete_input_buf(void)
 {
 	while (getchar() != '\n');
 }
+void read_arr(FILE * protein_seq, char * arr, int to_message)
+{
+	int index = 0;
+	
+	while (1)
+	{		
+		arr[index] = fgetc(protein_seq);
+		printf("%c", arr[index]);
+		if (arr[index] == to_message)
+		{
+			arr[index] == NULL;
+			break;
+		}		
+		index++;
+	}
+}
 int main()
 {
 	FILE* protein_seq = fopen("sequence.txt", "rt");
@@ -17,84 +33,59 @@ int main()
 	char search_id[20] = { 0 }; //검색할 이름
 	puts("이름을 입력하시오");
 	scanf("%s", search_id); //파일로부터 문자열을 읽어들인다.
-	
-	
 	delete_input_buf();
 
 	while (1)
 	{
 		char seq_id[200] = { 0 };
 		int i = 0;
-
+		index = 0;		
 		seq_id[i] = fgetc(protein_seq);
 		//fscanf(protein_seq, "%s", sequence);
 		if (seq_id[i] == '>')
-		{
-			printf("here\n");
-			index = 0;
+		{		
 			while (1)
-			{				
+			{
 				seq_id[index] = fgetc(protein_seq);
-		
 				if (seq_id[index] == 32)//스페이스바
-				{				
-					
+				{					
 					seq_id[index] = NULL;
+					printf("seq : %s\n", seq_id);
+					printf("search : %s\n", search_id);
 					if (!strcmp(seq_id, search_id))
 					{
-						
-						while (1)
-						{
-							char seq_info[100] = { 0 };
-							fscanf(protein_seq, "%s", seq_info);
-							
-							if (seq_info[0] == 'M')
-							{
-								break;
-							}
-							
-							
-							printf("%s", seq_info);
-						}
+						printf("same string\n");
+						char seq_info[100] = { 0 };
+						read_arr(protein_seq, seq_info, '\n');
 						printf("\n");
-						while (1)
-						{
-							char sequence[200] = { 0 };
-							fscanf(protein_seq, "%s", sequence);
-							if (sequence[0] == '>')
-							{
-								return 0;
-							}
-							printf("%s", sequence);
-						}
+						char sequence[1000] = { 0 };
+						read_arr(protein_seq, sequence, '*');
+						return 0;
 					}
-					/*else if (strcmp(seq_id, search_id))
+					else if (strcmp(seq_id, search_id))
 					{
-						printf("break!\n");
+						/*if (feof(protein_seq) != 0)
+						{
+							puts("NOT FOUND");
+						}*/
+						printf("break\n");
 						break;
 					}
-					else if (feof(protein_seq) != 0)
-					{
-						puts("NOT FOUND");
-					}*/
-
 				}
-				index++;
+				index++;				
 			}
 			i++;
 		}
-		else
+		/*else
 		{
 			continue;
-		}
-		//이름을 저장하는 과정
-
-
-		fseek(protein_seq, 0, SEEK_SET);
+		}*/
+		/*fseek(protein_seq, 0, SEEK_SET);
 		if (feof(protein_seq) != 0)
 		{
 			break;
-		}
+		}*/
 	}
+	fclose(protein_seq);
 	return 0;
 }
